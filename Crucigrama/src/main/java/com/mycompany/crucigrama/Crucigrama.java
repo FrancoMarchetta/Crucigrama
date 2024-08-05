855bd6e13c23e9b1e1fb86195c6309cb271373ea
Franco <frncmarchetta@gmail.com>
2024-08-04 21:06:12 -0300

    primera estructuracion del projecto

---
+++ Crucigrama/src/main/java/com/mycompany/crucigrama/Crucigrama.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crucigrama;
+
+/**
+ *
+ * @author frncm
+ */
+public class Crucigrama {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
